25e0a953b7e52fd1e50c310b11bd3ea608bd32b9
Andrea Dal Ponte <dalpo85@gmail.com>
2011-12-14 00:51:42 +0100

    behavior base logic

--- Model/Behavior/L10nFieldBehavior.php
+++ Model/Behavior/L10nFieldBehavior.php
@@ -9,7 +9,7 @@ class L10nFieldBehavior extends ModelBehavior {
 
   protected $_options = array();
 
-  protected $_dataTypes = array('date', 'datetime', 'time', 'timestamp');
+  protected $_l10nDataTypes = array('datetime', 'timestamp', 'date');
 
   protected $_ignoredFields = array('created', 'updated', 'modified');
 
@@ -17,94 +17,48 @@ class L10nFieldBehavior extends ModelBehavior {
 
   private $__l10nFields = array();
 
+  private $__dataTypes = array();
 
-  public function setup(Model &$model, $options = array()) {
 
-    $this->_options[$model->name] = array_merge($this->_defaultOptions, $options);
-    $this->_Model =& $model;
-    $this->__connection =& ConnectionManager::getDataSource($this->_model->useDbConfig);
-    
-    foreach($this->__connection->columns as $field => $info) {
-      if(isset($info['format']) && in_array($info['format'], $this->_dataTypes)) {
-        $this->__l10nFields[$this->_Model->name][$field] = $info['format'];
+  public function setup(Model &$Model, $options = array()) {
+
+    $this->_options[$Model->alias] = array_merge($this->_defaultOptions, $options);
+    $this->_Model =& $Model;
+    $this->__connection =& ConnectionManager::getDataSource($this->_Model->useDbConfig);
+
+    foreach($this->_Model->getColumnTypes() as $field => $dataType) {
+      if(in_array($dataType, $this->_l10nDataTypes) && !in_array($field, $this->_ignoredFields)) {
+        $this->__l10nFields[$this->_Model->alias][$field] = $dataType;
+      }
+    }
+
+    foreach($this->__connection->columns as $dataType => $info) {
+      if(isset($info['format'])) {
+        $this->__dataTypes[$this->_Model->alias][$dataType] = $info['format'];
       }
     }
   }
 
   public function getOptions() {
-    return $this->_options[$this->_Model->name];
+    return $this->_options[$this->_Model->alias];
   }
 
   public function getL10nFields() {
-    return $this->__localizedFields[$this->_Model->name];
+    return $this->__l10nFields[$this->_Model->alias];
   }
 
-
-
-
-  /**********************************/
+  public function getDataType() {
+    return $this->__dataTypes[$this->_Model->alias];
+  }
   
-  private $_columnTypes;
-  
-  private $__cakeAutomagicFields = array('created', 'updated', 'modified');
-  
-  private $__typesFormat;
-
-  public function setup(Model &$model, $options = array()) {
-    $this->settings = array(
-      'ignoreAutomagic' => true
-    );
-    
-    $this->_Model =& $model;
-    $this->_columnTypes = $this->_Model->getColumnTypes();
-    
-    
-    $db =& ConnectionManager::getDataSource($this->_model->useDbConfig);
-    
-    foreach($db->columns as $type => $info)
-    {
-      if(isset($info['format']))
-      {
-        $this->__typesFormat[$type] = $info['format'];
-      }
+  public function beforeSave(Model &$Model) {
+    parent::beforeSave($Model);
+    $this->_Model =& $Model;
+    $dataTypes = $this->getDataType();
+    foreach($this->getL10nFields() as $field => $dataType) {
+      //delocalize method!
     }
   }
-
-  public function beforeValidate(Model &$model)
-  {
-    $this->_model =& $model;
-    $this->_modelFields = $this->_model->getColumnTypes();
-
-    parent::beforeValidate($model);
-    
-    return $this->localizeData();
-  }
   
-  public function beforeSave(Model &$model)
-  {
-    $this->_model =& $model;
-    $this->_modelFields = $this->_model->getColumnTypes();
-
-    parent::beforeSave($model);
-    
-    return $this->localizeData();
-  }
-  
-  /**
-   * Invoca localização das informações no callback beforeFind
-   * 
-   * @see ModelBehavior::beforeFind()
-   */
-  public function beforeFind(&$model, $query)
-  {
-    $this->_model =& $model;
-    $this->_modelFields = $this->_model->getColumnTypes();
-    
-    parent::beforeFind($model, $query);
-    
-    $this->localizeData($query['conditions']);
-    
-    return $query;
-  }
   
 }
